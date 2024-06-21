d3daae201e51a9ebb7b5f0ad9589b97d5ee44d0e
Harsith R <96562342+ILikeHerculePoirot@users.noreply.github.com>
2024-06-21 17:57:32 +0530

    Modified GUI, Added Save As option

--- Ten.py
+++ Ten.py
@@ -27,11 +27,18 @@ def save():
         text_file.seek(0,2)
         text_file.write(text)
     else:
-        text_file = filedialog.asksaveasfile(mode="w",defaultextension=".txt",filetypes=[("Txt Files","*.txt")])
-        if(text_file):
-            global original_text
-            original_text = ""
-            file_active = True
+        saveAs()
+def saveAs():
+    global file_active,text_file
+    text = textBox.get("1.0",tk.END)
+    text_file = filedialog.asksaveasfile(mode="w",defaultextension=".txt",filetypes=[("Txt Files","*.txt")])
+    if(text_file):
+        global original_text
+        original_text = ""
+        file_active = True
+        text_file.truncate(0)
+        text_file.seek(0,2)
+        text_file.write(text)
 def revertOriginal():
     if(file_active):
         text_file.truncate(0)
@@ -55,42 +62,36 @@ def copy():
         root.clipboard_append(textBox.get(tk.SEL_FIRST,tk.SEL_LAST))
 def paste():
     textBox.insert(tk.INSERT,root.clipboard_get())
-def exit():
-    if(file_active):
-        text_file.close()
-    root.destroy()
 root = tk.Tk()
-root.geometry("793x581")
+root.geometry("800x600")
 root.title("Ten")
 icon = Image.open("Assets/Ten.ico")
 photo = ImageTk.PhotoImage(icon)
 root.wm_iconphoto(False,photo)
 root.resizable(False,False)
-root.configure(background="black")
+root.configure(background="SystemButtonFace")
 textFont = tk.font.Font(family="Times new roman",size=16)
-newButton = tk.Button(root,text="New",width=8,command=new)
-openExistingButton = tk.Button(root,text="Open Existing",command=openExisting)
-saveButton = tk.Button(root,text="Save",width=8,command=save)
-restoreOriginalButton = tk.Button(root,text="Restore Original",command=revertOriginal)
-selectAllButton = tk.Button(root,text="Select All",width=8,command=selectAll)
-cutButton = tk.Button(root,text="Cut",width=8,command=cut)
-copyButton = tk.Button(root,text="Copy",width=8,command=copy)
-pasteButton = tk.Button(root,text="Paste",width=8,command=paste)
-exitButton = tk.Button(root,text="Exit",width=8,command=exit)
-textBox = tk.Text(root,height=24,width=70,font=textFont,wrap="none")
-scrollbar_x = tk.Scrollbar(root,orient=tk.HORIZONTAL,command=textBox.xview)
+newButton = tk.Button(root,text="New",width=8,bd=0,command=new)
+openExistingButton = tk.Button(root,text="Open Existing",bd=0,command=openExisting)
+saveButton = tk.Button(root,text="Save",width=8,bd=0,command=save)
+saveAsButton = tk.Button(root,text="Save As",width=8,bd=0,command=save)
+restoreOriginalButton = tk.Button(root,text="Restore",width=8,bd=0,command=revertOriginal)
+selectAllButton = tk.Button(root,text="Select All",width=8,bd=0,command=selectAll)
+cutButton = tk.Button(root,text="Cut",width=8,bd=0,command=cut)
+copyButton = tk.Button(root,text="Copy",width=8,bd=0,command=copy)
+pasteButton = tk.Button(root,text="Paste",width=8,bd=0,command=paste)
+textBox = tk.Text(root,height=25,width=71,font=textFont,wrap="none")
 scrollbar_y = tk.Scrollbar(root,orient=tk.VERTICAL,command=textBox.yview)
-textBox.configure(xscrollcommand=scrollbar_x.set,yscrollcommand=scrollbar_y.set,tabs=textFont.measure(" " * 4),insertwidth=1)
-newButton.place(x=2,y=5)
-openExistingButton.place(x=67,y=5)
-saveButton.place(x=150,y=5)
-restoreOriginalButton.place(x=215,y=5)
-selectAllButton.place(x=309,y=5)
-cutButton.place(x=374,y=5)
-copyButton.place(x=439,y=5)
-pasteButton.place(x=504,y=5)
-exitButton.place(x=569,y=5)
-textBox.place(x=2,y=32)
-scrollbar_x.place(x=2,y=564,width=796)
-scrollbar_y.place(x=776,y=32,height=568)
+textBox.configure(yscrollcommand=scrollbar_y.set,tabs=textFont.measure(" " * 4),insertwidth=1,bd=0)
+newButton.place(x=0,y=3)
+openExistingButton.place(x=65,y=3)
+saveButton.place(x=148,y=3)
+saveAsButton.place(x=213,y=3)
+restoreOriginalButton.place(x=278,y=3)
+selectAllButton.place(x=343,y=3)
+cutButton.place(x=408,y=3)
+copyButton.place(x=473,y=3)
+pasteButton.place(x=538,y=3)
+textBox.place(x=0,y=26)
+scrollbar_y.place(x=780,y=26,height=572)
 root.mainloop()
